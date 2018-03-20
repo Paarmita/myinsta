489b10c8f15be2e6af8fa1c90cda6c4e48c97d9a
paarmita <paarmita1998@gmail.com>
2018-03-20 10:59:44 +0530

    added styling

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,61 +1,59 @@
-#require 'pry'
-
 class PicsController < ApplicationController
+    before_action :find_pic, only: [:show, :edit, :update, :destroy, :upvote]
+    before_action :authenticate_user!, except: [:index, :show]
+    
+    def index
+        @pics = Pic.all.order("created_at DESC")
+    end
 
-	before_action :find_pic, only: [:show, :edit, :update, :destroy]
-	# find pic only when these actions occur
-	def index
-		@pics = Pic.all.order("created_at Desc")	
-		#in descending order
-	end
+    def show
+    end
 
-	def show
-		# @pic = Pic.find(params[:id]) we need this for delete, edit, update nd other functions
-	end
+    def new
+        @pic = current_user.pics.build
+    end
+    
+    def create
+        @pic = current_user.pics.build(pic_params)
 
-	def new
-		@pic = current_user.pics.build
-		#to assign a user to a created pic
-	end
+        if @pic.save
+            redirect_to @pic, notice: "Yes! It was posted!"
+        else
+            render 'new'
+        end
+        
+    end
 
-	def create
-		@pic = current_user.pics.build(pic_params)
-		# if pic is saved notify otherwise take to new
-		if @pic.save
-			redirect_to @pic, notice: "Yes,It was posted!"
-		else
-			render 'new'
-		end
-	end
+    def edit
+    end
+    
+    def update
+        if @pic.update(pic_params)
+            redirect_to @pic, notice: "Congrats! Pic was updated!"
+        else
+            render 'edit'
+        end
+        
+    end
 
-	def edit
-	end
+    def destroy
+        @pic.destroy
+        redirect_to root_path
+    end
+    
+    def upvote
+        @pic.upvote_by current_user
+        redirect_to :back
+    end
+    
+    
+    private
 
-	def update
-		if @pic.update(pic_params)
-			redirect_to @pic, notice: "Congrats Pic was updated"
-		else
-			render'edit'      #render prevents data to get lost if pic not updated
-		end
-	end 
+    def pic_params
+        params.require(:pic).permit(:title, :description, :image)
+    end
 
-	def destroy
-		@pic.destroy
-		redirect_to root_path
-	end
-
-
-	# private methods are for internal usage within the defining class
-	private				# defining pics_params 
-
-	def pic_params
-		params.require(:pic).permit(:title, :discription)  # until add permit to add images 
-	end
-
-	def find_pic
-		@pic = Pic.find(params[:id])
-	end
-
+    def find_pic
+        @pic = Pic.find(params[:id])
+    end
 end
-	
-
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pics				#reffered to index.html in pics
+  resources :pics do 				#reffered to index.html in pics
+    member do
+      put "like", to: "pics#upvote"
+    end
+  end	
+
   root "pics#index"				#check by rake routes command 
 end
